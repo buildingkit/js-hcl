b5c6b49f27569eeb97f4733936cb653492de4048
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2020-08-20 20:39:03 +0200

    Add uglifyjs

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# build using gopherjs
 go get golang.org/dl/go1.12.16
 go1.12.16 download
 export GOPHERJS_GOROOT="$(go1.12.16 env GOROOT)"
 go mod vendor
-npm run build
+gopherjs build ./src -o dist/index.js -m
+
+# minify using uglifyjs
+$(npm bin)/uglifyjs --compress --mangle -o dist/index.js -- dist/index.js

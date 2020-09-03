01b93d6b8dd3525e47248b4207b2a16b5412c1df
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2020-09-03 18:16:52 +0200

    Try to add it to build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ go get golang.org/dl/go1.12.16
 go1.12.16 download
 export GOPHERJS_GOROOT="$(go1.12.16 env GOROOT)"
 go mod vendor
-gopherjs build ./src -o dist/index.js -m
+PATH=${PATH}:`go env GOPATH`/bin gopherjs build ./src -o dist/index.js -m
 
 # minify using uglifyjs
 $(npm bin)/uglifyjs --compress --mangle -o dist/index.js -- dist/index.js
